e8d3afbb09c1ce39749c5081b668ecfa2003c222
NeoGoHan-вум <neogohan1@yandex.com>
2025-03-06 22:54:06 +0300

    заготовка

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,7 +1,18 @@
 public class Race {
-    String winner;
-    int distance;
-    public void finisher() {
+    private String leaderName = "";
+    private int leaderDistance = 0;
 
+    public void updateLeader(Automobile car) {
+        int distance = 24 * car.getSpeed();
+
+        if (distance > leaderDistance) {
+            leaderDistance = distance;
+            leaderName = car.getName();
+        }
+    }
+
+    public String getLeaderName() {
+        return leaderName;
     }
 }
+
